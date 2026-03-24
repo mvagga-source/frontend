2d33992005f6d68dcc97a36f7e90c8acc040db92
m.vagga <m.vagga@gmail.com>
2026-03-24 16:40:07 +0900

    20260324

--- src/components/ScrollToTopButton/ScrollToTopButton.js
+++ src/components/ScrollToTopButton/ScrollToTopButton.js
@@ -5,6 +5,13 @@ const ScrollToTopButton = () => {
 
   const [visible, setVisible] = useState(false);
 
+  const UpArrow = () => (
+    <svg width="24" height="30" viewBox="0 0 24 24" fill="none">
+        <path d="M12 19V5" stroke="currentColor" strokeWidth="3" strokeLinecap="round" />
+        <path d="M5 12L12 5L19 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 200) {
@@ -29,7 +36,7 @@ const ScrollToTopButton = () => {
   return (
     visible && (
       <button onClick={scrollToTop} className="scrollToTop scrollToTop-ongoing-all">
-        ↑
+        <UpArrow/>
       </button>
     )
   );
