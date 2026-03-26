4c84c2868070027784f017d452a69f49170916a9
rladudals1234 <goadiddl@naver.com>
2026-03-26 19:48:34 +0900

    ???

--- src/pages/Qna/QnaApi.js
+++ src/pages/Qna/QnaApi.js
@@ -5,4 +5,4 @@ export const getQnaListApi = (page = 1, size = 10, searchParams = {}) => {
   return axiosInstance.get(`${process.env.REACT_APP_API_URL}/qna/list`, {
     params: { page, size, ...searchParams }
   });
-};
+}; 
